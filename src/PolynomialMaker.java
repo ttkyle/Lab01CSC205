b60dd1ed9bb236ba313926c3a77ad0d22caaa34a
kyle bentley <ttkyle@gmail.com>
2013-01-25 15:56:08 -0500

    Fixed Polynomial.java

--- src/PolynomialMaker.java
+++ src/PolynomialMaker.java
@@ -7,26 +7,47 @@ import java.util.Scanner;
  * Exercise: 1.26
  */
 public class PolynomialMaker {
+    private static Polynomial myPoly;
 
-    public PolynomialMaker() {
-
-    }
 
     public static void makePolynomial() {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the degree of the polynomial and " +
                 "the coefficient (highest degree to lowest)");
-
-
     }
 
     public static void main(String[] args) {
-        //makePolynomial();
-        for(String s: args) {
-
+        int count = 0;
+        myPoly = new Polynomial(Integer.parseInt(args[0]));
+        for(int n = 1; n < args.length; n++) {
+            if((Integer.parseInt(args[n]) != 0)) {
+                count++;
+            }
         }
-        for(int i = 0; i < args.length; i++) {
-            String firstArg = args[i];
+        for(int i = 1; i < args.length; i++) {
+            myPoly.setCoefficients(count,Integer.parseInt(args[i]));
+            count--;
+        }
+
+        askUser();
+        //System.out.println(myPoly.evaluate(0));
+        //System.out.println(myPoly.evaluate(1));
+        //System.out.println(myPoly.evaluate(0.5));
+    }
+
+    public static void askUser() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a value>");
+        int value = input.nextInt();
+        System.out.println("The value is " + myPoly.evaluate(value));
+
+        System.out.println("Continue?>");
+        String valueYesNo = input.next();
+        if(valueYesNo.equals("Yes") || valueYesNo.equals("yes") || valueYesNo.equals("y")) {
+            askUser();
+        }
+        else {
+            System.exit(0);
         }
     }
 }
