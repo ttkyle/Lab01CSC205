97dcab3e3cfd722b9fe49e6515779f724abc309c
kyle bentley <ttkyle@gmail.com>
2013-01-25 16:21:07 -0500

    Fixed Polynomial.java

--- src/CardDriver.java
+++ src/CardDriver.java
@@ -8,13 +8,16 @@ public class CardDriver {
 
     public static void main(String[] args) {
         Card card = new Card(1,4);
-
-        System.out.println(card.getSuit());
         System.out.println(card);
+        System.out.println("Card is suit " + card.getSuit());
+        System.out.println("Card is rank " + card.getRank());
+
+
+        System.out.println();
 
         Card card2 = new Card(5, 3);
-
-        System.out.println(card2.getSuit());
         System.out.println(card2);
+        System.out.println("Card is suit " +card2.getSuit());
+        System.out.println("Card is rank " + card2.getRank());
     }
 }
