6026263472cbd57f1cc95ec3b9d3ac72fab33313
kyle bentley <ttkyle@gmail.com>
2013-01-31 15:38:49 -0500

    Fixed Polynomial.java Fixed ShoppingBagRepeat.java fixed the card shit maybe

--- src/CardDriver.java
+++ src/CardDriver.java
@@ -9,15 +9,15 @@ public class CardDriver {
     public static void main(String[] args) {
         Card card = new Card(1,4);
         System.out.println(card);
-        System.out.println("Card is suit " + card.getSuit());
-        System.out.println("Card is rank " + card.getRank());
+        System.out.println("Card one is suit " + card.getSuit());
+        System.out.println("Card one is rank " + card.getRank());
 
 
         System.out.println();
 
         Card card2 = new Card(5, 3);
         System.out.println(card2);
-        System.out.println("Card is suit " +card2.getSuit());
-        System.out.println("Card is rank " + card2.getRank());
+        System.out.println("Card two is suit " +card2.getSuit());
+        System.out.println("Card two is rank " + card2.getRank());
     }
 }
